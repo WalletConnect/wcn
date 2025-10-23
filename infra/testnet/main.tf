8fe2a87f1ff5018975c4d4f1a001d37482880910
Darksome <dev@darkso.me>
2025-10-23 16:11:11 +0000

    specify providers

--- infra/testnet/main.tf
+++ infra/testnet/main.tf
@@ -8,6 +8,30 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "eu-central-1"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+  alias  = "eu"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+  alias  = "ap"
+}
+
+provider "aws" {
+  region = "sa-east-1"
+  alias  = "sa"
+}
+
 module "sops-encryption-key" {
   source = "../modules/sops-encryption-key"
 }
