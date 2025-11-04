06da23e03f5ebbc327f1f48f25d0a82d1be6d7fd
Darksome <dev@darkso.me>
2025-11-04 14:23:33 +0000

    feat(rpc): inbound connection metric gauge

--- crates/replication/src/coordinator/mod.rs
+++ crates/replication/src/coordinator/mod.rs
@@ -215,7 +215,7 @@ impl<C: Config> StorageApi for InboundConnection<C> {
         }
         .unwrap_or_else(|| {
             metrics::counter!("wcn_replication_coordinator_inconsistent_results").increment(1);
-            tracing::warn!(?responses, "Inconsistent results");
+            tracing::debug!(?responses, "Inconsistent results");
             const { &Err(Error::new(ErrorKind::Internal)) }
         });
 
