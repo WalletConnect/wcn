e80984af245385c18227022f6e2bea17d98972b3
Darksome <dev@darkso.me>
2025-12-03 20:08:41 +0000

    fix: grafana config

--- infra/modules/node-operator/main.tf
+++ infra/modules/node-operator/main.tf
@@ -280,7 +280,7 @@ module "grafana_prometheus_datasource_config" {
       basicAuth = true
       basicAuthUser = "grafana"
       secureJsonData = {
-        basicAuthPassword = local.grafana_prometheus_file_path
+        basicAuthPassword = local.grafana_prometheus_password_file_path
       }
     }]
   })
