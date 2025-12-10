80f85b19b25b705e3183d7512d38a2ea221b916b
Darksome <dev@darkso.me>
2025-12-10 18:46:40 +0000

    fix: disable https gateway healthcheck

--- infra/modules/https-gateway/main.tf
+++ infra/modules/https-gateway/main.tf
@@ -63,6 +63,9 @@ resource "aws_lb_target_group" "this" {
   protocol    = "HTTP"
   vpc_id      = var.vpc.vpc_id
   target_type = "ip"
+  health_check {
+    enabled = false
+  }
 }
 
 resource "aws_lb_target_group_attachment" "this" {
