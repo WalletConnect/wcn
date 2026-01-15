fc06c0a39633ad0c52f33522103b4ca4d02f5819
Darksome <dev@darkso.me>
2026-01-15 11:02:34 +0000

    fmt

--- infra/testnet/main.tf
+++ infra/testnet/main.tf
@@ -110,7 +110,7 @@ locals {
   eu_operators = {
     wallet-connect = {
       vpc_cidr_octet = 105 # 10.105.0.0/16
-      db = local.db_config
+      db             = local.db_config
       nodes = [
         local.node_config,
         local.node_config,
