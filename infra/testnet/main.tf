e422771b7bcb98ede4d2d67d73a2b388957cab8a
Darksome <dev@darkso.me>
2026-01-15 10:31:48 +0000

    switch operator-a db to x86

--- infra/testnet/main.tf
+++ infra/testnet/main.tf
@@ -123,7 +123,7 @@ locals {
 
     operator-a = {
       vpc_cidr_octet = 0 # 10.0.0.0/16
-      db             = local.db_config
+      db = merge(local.db_config, { cpu_arch = "x86" })
       nodes = [
         local.node_config,
         local.node_config,
