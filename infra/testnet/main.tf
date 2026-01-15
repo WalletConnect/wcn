702042821ba24eaa8f41a9e8327a6b7b9d9fd737
Darksome <dev@darkso.me>
2026-01-15 10:20:53 +0000

    switch operator-b db to arm

--- infra/testnet/main.tf
+++ infra/testnet/main.tf
@@ -132,7 +132,7 @@ locals {
 
     operator-b = {
       vpc_cidr_octet = 0 # 10.0.0.0/16
-      db             = local.db_config
+      db = merge(local.db_config, { cpu_arch = "x86" })
       nodes = [
         local.node_config,
         local.node_config,
